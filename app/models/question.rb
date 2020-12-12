9447bb7b7481b8fc06f9cd41fbbc6f76c0247259
Aleksandr Karyakin <kng.sasha@mail.ru>
2020-12-12 17:24:55 +0300

    add Answer with test for reference whit Questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ApplicationRecord
+  has_many :answers
+
   validates :title, :body, presence: true
+  validates :title, length: { minimum: 5 }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   it { should validate_presence_of :title }
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should have_many(:answers) }
   it { should validate_presence_of :body }
 
 end
