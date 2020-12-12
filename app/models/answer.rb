85aac5edc794449f2f84905966eab050aa9a0748
Aleksandr Karyakin <kng.sasha@mail.ru>
2020-12-12 18:03:58 +0300

    Add Validation for question_id inside Answer with test

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,5 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true, length: { minimum: 5 }
+  validates :question_id, presence: true
 end
---
+++ db/migrate/20201212145704_add_not_nul_in_answers.rb
@@ -0,0 +1,5 @@
+class AddNotNulInAnswers < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null :answers, :question_id, false
+  end
+end
