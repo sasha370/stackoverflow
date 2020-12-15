3a702db832edbf07f7e4104a6b5ef56045b64640
Aleksandr Karyakin <kng.sasha@mail.ru>
2020-12-15 18:50:01 +0300

    add spec for User.author?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it {should validate_presence_of :email}
-  it {should validate_presence_of :password}
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :password }
+
+  describe 'author?' do
+    let!(:user) { create(:user) }
+    let!(:question) { create(:question, user: user) }
+    let(:another_user) { create(:user) }
+
+    it 'should be correct if user is author' do
+      expect(user.author?(question)).to eq(true)
+    end
+
+    it 'shouldn`t not be correct if user are not author' do
+      expect(another_user.author?(question)).to eq(false)
+    end
+  end
 end
