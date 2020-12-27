e1e050a21f390fb74692cdb4e9de0fded83850e2
Aleksandr Karyakin <kng.sasha@mail.ru>
2020-12-27 15:36:29 +0300

    add lincable concern

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,15 +1,13 @@
 class Answer < ApplicationRecord
   include Ratingable
+  include Linkable
 
   belongs_to :question
   belongs_to :user
-  has_many :links, dependent: :destroy, as: :linkable
-  has_many :ratings, dependent: :destroy, as: :ratingable
 
   has_many_attached :files
 
   validates :body, presence: true, length: { minimum: 5 }
-  accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
 
   scope :sort_by_best, -> { order(best: :desc) }
   scope :best, -> { where(best: true) }
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,15 +1,14 @@
 class Question < ApplicationRecord
   include Ratingable
+  include Linkable
 
   has_many :answers, dependent: :destroy
-  has_many :links, dependent: :destroy, as: :linkable
   belongs_to :user
   has_one :reward, dependent: :destroy
 
   has_many_attached :files
 
   accepts_nested_attributes_for :reward, reject_if: :all_blank
-  accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
 
   validates :title, :body, presence: true
   validates :title, length: { minimum: 5 }
