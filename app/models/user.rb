c9063101a5ad4fd1ec9c646c7bd9579a6b804b4e
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-01-13 20:06:08 +0300

    test2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,6 +9,7 @@ class User < ApplicationRecord
   has_many :answers
   has_many :rewards
   has_many :ratings
+  has_many :authorizations, dependent: :destroy
 
   def author?(resource)
     id == resource.user_id
@@ -17,7 +19,11 @@ class User < ApplicationRecord
     resource.ratings.exists?(user_id: id)
   end
 
-  def self.find_for_oauth(data)
+  def self.find_for_oauth(auth)
+    FindForOauthService.new(auth).call
+  end
 
+  def create_authorization(auth)
+    self.authorizations.create(provider: auth[:provider], uid: auth[:uid].to_s)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :password }
   it { should have_many(:rewards) }
   it { should have_many(:ratings) }
+  it { should have_many(:authorizations).dependent(:destroy) }
 
   describe 'author?' do
     let!(:user) { create(:user) }
@@ -22,6 +23,32 @@ RSpec.describe User, type: :model do
     it 'already add voice for resource' do
       question.vote_plus(user)
       expect(user.voted?(question)).to eq true
+    end
+  end
+
+  describe '.find_for_oauth' do
+    let!(:user) { create(:user, email: 'jackson.keeling@example.com') }
+    let(:auth) { Faker::Omniauth.github }
+
+    context 'user already has authorization' do
+      it 'returns the user' do
+        user.authorizations.create(provider: auth[:provider], uid: auth[:uid])
+        expect(User.find_for_oauth(auth)).to eq user
+      end
+    end
+
+    context 'user has not authorization' do
+      context 'user already exist' do
+        let!(:auth) { Faker::Omniauth.github }
+        let!(:user) { create(:user, email: auth[:info][:email]) }
+        let(:service) { double('FindForOauthService') }
+
+        it 'calls FindForOauthService' do
+          expect(FindForOauthService).to receive(:new).with(auth).and_return(service)
+          expect(service).to receive(:call)
+          User.find_for_oauth(auth)
+        end
+      end
     end
   end
 end
