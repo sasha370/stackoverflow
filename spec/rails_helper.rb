a7bd8bced45e71fa9307ae05f12660b6d1963d0d
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-01-14 21:36:08 +0300

    test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,10 +48,11 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include ControllerHelpers, type: :controller
   config.include FeatureHelpers, type: :feature
   config.include ActiveStorageHelpers
-  config.include OmniauthHelpers
+  config.include OmniauthHelpers, type: :feature
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
