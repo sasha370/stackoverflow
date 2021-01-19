36419138beb7cc59acb94db5d9c73291747deb3d
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-01-19 15:28:05 +0300

    can create answer

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -12,4 +12,21 @@ class Api::V1::AnswersController < Api::V1::BaseController
     @answer = Answer.find(params[:id])
     render json: @answer
   end
+
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new(answer_params)
+    @answer.user = current_resource_owner
+    if @question.save
+      render json: @answer
+    else
+      head 422
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
 end
--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -39,7 +39,7 @@ describe 'Questions API', type: :request do
     let!(:question) { create(:question) }
     let!(:answer) { create(:answer, :with_file, question: question) }
     let!(:comments) { create_list(:comment, 3, commentable_type: 'Answer', commentable_id: answer.id) }
-    let(:answer_responce) { json['answer'] }
+    let(:answer_response) { json['answer'] }
 
     let(:api_path) { "/api/v1/answers/#{answer.id}" }
 
@@ -62,42 +62,83 @@ describe 'Questions API', type: :request do
 
       it 'returns public fields' do
         %w[id body created_at updated_at question_id best].each do |attr|
-          expect(answer_responce[attr]).to eq answer.send(attr).as_json
+          expect(answer_response[attr]).to eq answer.send(attr).as_json
         end
       end
 
       it 'contains user object' do
-        expect(answer_responce['user']['id']).to eq answer.user.id
+        expect(answer_response['user']['id']).to eq answer.user.id
       end
 
       it 'contains list of links' do
-        expect(answer_responce['links'].size).to eq answer.links.count
-        expect(answer_responce['links'].first['id']).to eq answer.links.first.id
+        expect(answer_response['links'].size).to eq answer.links.count
+        expect(answer_response['links'].first['id']).to eq answer.links.first.id
       end
 
       it 'contains list of attachments' do
-        expect(answer_responce['files'].size).to eq answer.files.count
-        expect(answer_responce['files'].first['id']).to eq answer.files.first.id
+        expect(answer_response['files'].size).to eq answer.files.count
+        expect(answer_response['files'].first['id']).to eq answer.files.first.id
       end
 
       it 'contains list of comments' do
-        expect(answer_responce['comments'].size).to eq answer.comments.count
-        expect(answer_responce['comments'].first['id']).to eq answer.comments.first.id
+        expect(answer_response['comments'].size).to eq answer.comments.count
+        expect(answer_response['comments'].first['id']).to eq answer.comments.first.id
+      end
+    end
+  end
+
+  describe 'POST /api/v1/questions/:id/answers' do
+    let!(:question) { create(:question) }
+    let(:api_path) { "/api/v1/questions/#{question.id}/answers" }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :post }
+    end
+
+    context 'authorized' do
+      let(:user) { create(:user) }
+      let(:access_token) { create(:access_token, resource_owner_id: user.id) }
+      let(:answer_attr) { attributes_for(:answer) }
+      let(:answer_attr_incorrect) { {body: ''} }
+      let(:answer_response) { json['answer'] }
+
+      context 'with correct data' do
+        before { post api_path, params: {answer: answer_attr, access_token: access_token.token}, headers: headers }
+
+        it 'return 200 status' do
+          expect(response).to be_successful
+        end
+
+        it 'save answer in DB' do
+          expect { post api_path, params: {answer: answer_attr, access_token: access_token.token}, headers: headers }.to change(Answer, :count).by(1)
+        end
+
+        it 'return answer' do
+          expect(['answer'].size).to eq 1
+        end
+
+        it 'return answer with correct user' do
+          expect(answer_response['user']['id']).to eq user.id
+        end
+
+        it 'returns correct fields' do
+          expect(answer_response['body']).to eq answer_attr[:body]
+        end
+      end
+
+      context 'with incorrect data' do
+
+        before { post api_path, params: {answer: answer_attr_incorrect, access_token: access_token.token}, headers: headers}
+
+        it 'don`t save question in DB' do
+          expect { post api_path, params: {answer: answer_attr_incorrect, access_token: access_token.token}, headers: headers}.to_not change(Answer, :count)
+        end
+
+        it 'return 422 status' do
+          expect(response).to have_http_status(422)
+        end
       end
     end
   end
 end
-# describe 'answers' do
-#   let(:answer) { answers.first }
-#   let(:answer_response) { question_response['answers'].first }
-#
-#   it 'return list of answers' do
-#     expect(question_response['answers'].size).to eq 3
-#   end
-#
-#   it 'returns public fields' do
-#     %w[id body user_id created_at updated_at].each do |attr|
-#       expect(answer_response[attr]).to eq answer.send(attr).as_json
-#     end
-#   end
-# end
+
