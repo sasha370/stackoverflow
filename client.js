a4f26a18d4a90c44d02a212b294fbe54fb7b29db
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-01-21 19:05:21 +0300

    edit workflow for github2

--- client.js
+++ client.js
@@ -12,20 +12,7 @@ redisClient.on("error", function(err) {
   console.log("Error " + err);
 });
 
-// Sets the key "octocat" to a value of "Mona the octocat"
-redisClient.set("octocat", "Mona the Octocat", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Cat and Octopus"
-redisClient.hset("species", "octocat", "Cat and Octopus", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Dinosaur and Octopus"
-redisClient.hset("species", "dinotocat", "Dinosaur and Octopus", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Cat and Robot"
-redisClient.hset(["species", "robotocat", "Cat and Robot"], redis.print);
-// Gets all fields in "species" key
 
-redisClient.hkeys("species", function (err, replies) {
-  console.log(replies.length + " replies:");
-  replies.forEach(function (reply, i) {
-    console.log("    " + i + ": " + reply);
-  });
-  redisClient.quit();
-});
+
+
+
