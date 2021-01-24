aaa7ad6fe3bdbdcc2eec46ec8685d6da02170dae
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-01-24 21:46:02 +0300

    repaire links for search results

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,21 +1,13 @@
 class SearchesController < ApplicationController
   before_action :search_params, only: [:search]
 
-
   def search
     redirect_to root_path, alert: 'Search can`t be blanc' if params[:query].blank?
-    if @search[:options] == 'all'
-      @results = ThinkingSphinx.search @search[:query]
-    else
-      @results = klass_name.search @search[:query]
-    end
+    @klass_name = @search[:options] == 'All' ? ThinkingSphinx : @search[:options].constantize
+    @results = @klass_name.search @search[:query]
   end
 
   private
-
-  def klass_name
-    @search[:options].capitalize.constantize
-  end
 
   def search_params
     @search = params.permit(:query, :options, :commit)
--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -3,17 +3,38 @@ require 'sphinx_helper'
 RSpec.describe SearchesController, type: :controller do
 
   describe 'GET #search' do
-    let!(:query) { {"query"=>"test search", "options"=>"all"} }
+    # let!(:search) { {"query" => "test search", "options" => "all"} }
+    let!(:question) { create(:question, title: 'First question') }
 
-    it 'assigns the requested query to @query' do
-      get :search, params: { query: 'test search', options: 'all'  }
-      expect(assigns(:query)).to eq query
+    it 'assigns the requested search to @search' do
+      get :search, params: {query: 'test search', options: 'All'}
+      expect(assigns(:search)).to eq( "query"=>"test search", "options"=>"All")
     end
 
-    it 'assigns the requested query to @query' do
-      get :search, params: { query: 'test search', options: 'question'  }
+    it 'assigns the requested klass_name to @klass_name ' do
+      get :search, params: {query: 'test search', options: 'Question'}
       expect(assigns(:klass_name)).to eq Question
     end
+
+    it 'assigns the requested ThinkingSphinx to @klass_name if All' do
+      get :search, params: {query: 'test search', options: 'All'}
+      expect(assigns(:klass_name)).to eq ThinkingSphinx
+    end
+
+    it 'the search engine responds for ALL query' do
+      allow(ThinkingSphinx).to receive(:search).with("test search")
+      get :search, params: {query: 'test search', options: 'All'}
+    end
+
+    it 'the search engine responds for ALL  query and return data' do
+      allow(ThinkingSphinx).to receive(:search).with("First").and_return(question)
+      get :search, params: {query: 'First', options: 'All'}
+    end
+
+    # it 'the search engine responds to the request by Class and returns it instance' do
+    #   get :search, params: {query: 'First', options: 'Question'}
+    #   expect(assigns(:results)).to be_instance_of Question
+    # end
 
     it 'redirect to root if query is empty' do
       get :search, params: {query: ''}
@@ -21,7 +42,7 @@ RSpec.describe SearchesController, type: :controller do
     end
 
     it 'render to Search if query is not empty' do
-      get :search, params: {query: 'test', options: 'all'}
+      get :search, params: {query: 'test', options: 'All'}
       expect(response).to render_template :search
     end
 
