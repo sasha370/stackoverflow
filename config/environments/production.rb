d367a93354b4e7f1f1c287171fc9398a30c71a9c
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-01-27 20:48:50 +0300

    repaire mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,12 +62,12 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "stackoverflow_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'http://88.214.237.55/', :protocol => 'https' }
+  config.action_mailer.default_url_options = { host: 'http://88.214.237.55', :protocol => 'http' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
-    domain: 'http://88.214.237.55/',
+    domain: 'http://88.214.237.55',
     port: 587,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
