f6dc548582b11b9e837fb6d761f46f98b5d807ea
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-01-28 20:47:07 +0300

    repaire mailer 11

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,6 @@ Rails.application.configure do
 
 
     config.action_controller.perform_caching = false
-
     config.action_mailer.perform_caching = false
     config.action_mailer.default_url_options = {host: '127.0.0.1'}
     config.action_mailer.delivery_method = :smtp
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.assets.enabled = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -13,7 +12,6 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
-  config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
