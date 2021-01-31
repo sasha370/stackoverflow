75b9114924a6c81c59feb7af950c107be51baf94
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-01-31 18:43:07 +0300

    rebuild precopmpile 2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css.scss, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+%w(eot svg ttf woff woff2).each do |ext|
+  Rails.application.config.assets.precompile << "fontawesome-webfont.#{ext}"
+end
